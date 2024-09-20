22f7830d66bd4454ebdc7d87c8b15b0aeed0ea9f
Marouan <marouan.yaouaddou@student.vinci.be>
2024-09-20 09:04:39 +0200

    1st tutorial

---
+++ tutorials/ts-node-start/index.ts
@@ -0,0 +1,15 @@
+const dacia= {
+    brand : "Dacia",
+    model : "Sandero",
+    id    : undefined,
+    getDescription: function () {
+        return `${this.id} ${this.brand} ${this.model}`;
+    },
+};
+
+console.log(dacia.getDescription());
+
+dacia.id = Math.random();
+
+console.log(dacia.getDescription());
+
