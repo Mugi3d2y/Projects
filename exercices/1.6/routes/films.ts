b22bf2f82a41d663ef4656daabca9cc4a05b232e
Marouan Yaou Addou <marouan.yaouaddou@student.vinci.be>
2024-10-14 14:26:32 +0200

    new: ex1.6

--- exercices/1.6/routes/films.ts
+++ exercices/1.6/routes/films.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { Film, NewFilm } from "../types";
 
+
 const router = Router();
 
 const defaultFilms: Film[] = [
@@ -158,7 +159,102 @@ router.delete("/:id", (req, res) =>{
 
   const deletedElements = defaultFilms.splice(index,1);
   res.json(deletedElements[0]);
-})
+});
+
+
+router.patch("/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  if(isNaN(id)){
+    return res.sendStatus(400);
+  }
+
+  const filmToUpdate = defaultFilms.find(film => film.id === id);
+
+  if(filmToUpdate===undefined){
+    return res.sendStatus(404);
+  }
+
+  const body: unknown = (req.body);
+
+ if (
+    !body ||
+    typeof body !== "object" ||
+    Object.keys(body).length === 0 ||
+    ("title" in body &&
+      (typeof body.title !== "string" || !body.title.trim())) ||
+    ("director" in body &&
+      (typeof body.director !== "string" || !body.director.trim())) ||
+    ("duration" in body &&
+      (typeof body.duration !== "number" || body.duration <= 0)) ||
+    ("budget" in body &&
+      (typeof body.budget !== "number" || body.budget <= 0)) ||
+    ("description" in body &&
+      (typeof body.description !== "string" || !body.description.trim())) ||
+    ("imageUrl" in body &&
+      (typeof body.imageUrl !== "string" || !body.imageUrl.trim()))
+  ) {
+    return res.sendStatus(400);
+  }
+
+    const updatedFilm = {...filmToUpdate,...body};
+
+    defaultFilms[defaultFilms.indexOf(filmToUpdate)] = updatedFilm;
+
+    return res.send(updatedFilm);
+});
+
+
+router.put("/:id", (req, res) =>{
+  const body: unknown = req.body;
+
+  if (
+    !body ||
+    typeof body !== "object" ||
+    !("title" in body) ||
+    !("director" in body) ||
+    !("duration" in body) ||
+    typeof body.title !== "string" ||
+    typeof body.director !== "string" ||
+    typeof body.duration !== "number" ||
+    !body.title.trim() ||
+    !body.director.trim() ||
+    body.duration <= 0 ||
+    ("budget" in body &&
+      (typeof body.budget !== "number" || body.budget <= 0)) ||
+    ("description" in body &&
+      (typeof body.description !== "string" || !body.description.trim())) ||
+    ("imageUrl" in body &&
+      (typeof body.imageUrl !== "string" || !body.imageUrl.trim()))
+  ) {
+    return res.sendStatus(400);
+  }
+
+  const id = Number(req.params.id);
+
+  if(isNaN(id)){
+    res.sendStatus(400);
+  }
+
+  const indexOfFilmToUpdate = defaultFilms.findIndex(film => film.id === id);
+
+  if(indexOfFilmToUpdate===-1){
+    const newFilm = body as NewFilm;
+    const nextId = defaultFilms.reduce((acc, film) => (film.id > acc ? film.id: acc), 0) + 1;
+    const addedFilm = {id : nextId, ...newFilm};
+    defaultFilms.push(addedFilm);
+    return res.json(addedFilm);
+  }
+
+  const filmToUpdate = {...defaultFilms[indexOfFilmToUpdate], ...body} as Film;
+
+  defaultFilms[indexOfFilmToUpdate] = filmToUpdate;
+
+  return res.send(filmToUpdate);
+
+
+
+});
 
 
 export default router;
